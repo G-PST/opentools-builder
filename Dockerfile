8cda5bd84d00a4cf66b84050b36d4c58641a7d95
Duwadi <kduwadi@nrel.gov>
2023-10-27 16:03:47 -0600

    added ability to download file from issue's last comment, create a branch and pull request

--- Dockerfile
+++ Dockerfile
@@ -1,17 +1,20 @@
-FROM python:3-slim AS builder
+FROM python:3-alpine
+#AS builder
 ADD . /app 
 WORKDIR /app
 
 # Installing dependency directly in 
 # the app directory
-RUN pip install --target=/app pydantic pytest
+
+RUN pip install --target=/app pydantic pytest requests
 
 # Let's use a distroless container image for 
 # ptrhon and some basic SSL certificates
 # https://github.com/GoogleContainerTools/distroless
 
-FROM gcr.io/distroless/python3-debian12
-COPY --from=builder /app /app 
+# FROM gcr.io/distroless/python3-debian12
+RUN apk update && apk add --no-cache git
+# COPY --from=builder /app /app 
 # WORKDIR /app 
 ENV PYTHONPATH /app 
 CMD ["/app/main.py"]
--- main.py
+++ main.py
@@ -2,13 +2,79 @@
 
 # standard imports
 import os
+import http
+import re
+import subprocess
 
 # third-party imports
 import pytest
+import requests
+
+
+def download_data_file(issue_url: str, access_token: str):
+    """Function to download issue file."""
+    headers = {"Authorization": f"token {access_token}"}
+    response = requests.get(f"{issue_url}/comments", headers=headers)
+
+    if response.status_code != http.HTTPStatus.OK:
+        return
+
+    comments = response.json()
+    if not comments:
+        return
+
+    json_files = [
+        item
+        for item in re.findall(r"(https?://[^\s)]+)", comments[-1]["body"])
+        if item.endswith(".json")
+    ]
+
+    if not json_files:
+        return
+
+    latest_json_file = json_files[-1]
+    file_name = latest_json_file.split("/")[-1]
+    file_response = requests.get(latest_json_file)
+
+    if file_response.status_code != http.HTTPStatus.OK:
+        return False
+
+    with open(file_name, "wb") as f:
+        f.write(file_response.content)
+
+    print(f"File {file_name} downloaded successfully.")
+    return file_name
 
 
 def main():
     """Entry function for github action."""
+    issue_number = os.environ["INPUT_ISSUE_NUMBER"]
+    issue_title = os.environ["INPUT_ISSUE_TITLE"]
+    issue_url = os.environ["INPUT_ISSUE_URL"]
+    token = os.environ["INPUT_TOKEN"]
+
+    if issue_number or issue_title or issue_url or token:
+        if not (issue_number and issue_title and issue_url and token):
+            print("::set-output name=exitcode::1")
+            return
+
+        os.environ["GITHUB_TOKEN"] = token
+        file_name = download_data_file(issue_url, token)
+        if not file_name:
+            print("::set-output name=exitcode::0")
+            return
+
+        branch_name = f"issue_{issue_number}_branch"
+        subprocess.run(["git", "checkout", "-b", branch_name], check=True)
+        subprocess.run(["git", "add", file_name], check=True)
+        commit_message = f"Add issue file: {file_name}"
+        subprocess.run(["git", "commit", "-m", commit_message], check=True)
+        subprocess.run(
+            ["git", "push", "--set-upstream", "origin", branch_name], check=True
+        )
+        print("::set-output name=exitcode::0")
+        return
+
     try:
         data_path = os.environ["INPUT_DATAPATH"]
     except KeyError as _:
